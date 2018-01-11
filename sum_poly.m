e56d07c92b664ddf7da25607e3d1734b4ae93d56
lenarttreven <lenart.treven@student.fmf.uni-lj.si>
2018-01-11 18:19:32 +0100

    reordering

--- sum_poly.m
+++
@@ -1,7 +0,0 @@
-function p3 = sum_poly(p1, p2)
-% sestje dva polinoma
-
-p3 =[zeros(1, size(p1,2)-size(p2,2)) p2] + [zeros(1, size(p2,2)-size(p1,2)) p1];
-
-end
-
--- tangentna.m
+++
@@ -1,6 +0,0 @@
-function [x,X,k] = tangentna(f,df,x0,tol,N)
-
-g = @(x) x - f(x)./df(x);
-[x,X,k] = iteracija(g,x0,tol,N);
-
-end
