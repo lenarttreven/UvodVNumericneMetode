a85afcfec8837d8020431601507164bc11575968
lenarttreven <lenart.treven@student.fmf.uni-lj.si>
2018-01-12 09:09:22 +0100

    corrections

--- domaca_naloga2/HW2_Problem2.m
+++ domaca_naloga2/HW2_Problem2.m
@@ -1,6 +1,6 @@
 close all
-f = @(x) x .* sin(3*x);
-df = @(x) sin(3*x) + 3*x.* cos(3*x);
+f = @(x) x .* sin(3 * x);
+df = @(x) sin(3*x) + 3 * x.* cos(3 * x);
 
 
 A = zeros(11, 4);
@@ -15,19 +15,16 @@ end
 
 % Sedaj re?ujemo predolo?en sistem Ax = b
 p = A\b;
-p1 = A(1:5,:)\b(1:5);
-p2 = A(7:11,:)\b(7:11);
+% p1 = A(1:5,:)\b(1:5);
+% p2 = A(7:11,:)\b(7:11);
 
 x = linspace(0, 2);
 
+subplot(1,3,1);
 plot(x, polyval(p, x))
 hold on 
 plot(x, f(x))
 hold on
-plot(x, polyval(p1, x))
-hold on 
-plot(x, polyval(p2, x))
-hold on 
 
 
 A1 = A(1:5,:);
@@ -52,13 +49,23 @@ S = [AA' * AA, CC'; CC , zeros(size(CC, 1),size(CC, 1))];
 u  = [AA' * bb ; dd];
 x1 = S\u;
 
-p3 = x1(1:4);
-p4 = x1(5:8);
+p1 = x1(1:4);
+p2 = x1(5:8);
 
-plot(x, polyval(p3, x))
-hold on 
-plot(x, polyval(p4, x))
-hold on 
+x1 = linspace(0,1);
+
+subplot(1,3,2); 
+plot(x1, polyval(p1, x1))
+hold on
+plot(x1, f(x1))
+
+x2 = linspace(1,2);
+
+subplot(1,3,3); 
+plot(x2, polyval(p2, x2))
+hold on
+plot(x2, f(x2))
+
 
 
 
