da0695d098b066af06e12ed35a20dd67da70e736
lenarttreven <lenart.treven@student.fmf.uni-lj.si>
2018-01-13 16:20:33 +0100

    corrections

--- domaca_naloga2/HW2_Problem3.m
+++ domaca_naloga2/HW2_Problem3.m
@@ -1,12 +1,12 @@
 n = 1000;
-koraki = 10;
+koraki = 100;
 
 A = rand(n);
-A = A' * A;
+B = A' * A;
 x0 = rand(n, 1);
 
-sigma_1 = potencna(A , x0, koraki);
-sigma_n = inverzna_potencna(A, x0, koraki);
-pogojenostno_stevilo = sigma_1 * sigma_n 
+sigma_1 = potencna(B , x0, koraki);
+sigma_n = inverzna_potencna(B, x0, koraki);
+pogojenostno_stevilo = sqrt(sigma_1 * sigma_n) 
 vgrajeno_pogojenostno_stevilo = cond(A)
-
+razlika = pogojenostno_stevilo - vgrajeno_pogojenostno_stevilo
